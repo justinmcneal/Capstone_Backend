0184495db85bf7041d81ef5ba36b78c548cc4463
justinmcneal <142385237+justinmcneal@users.noreply.github.com>
2026-06-10 23:11:19 +0800

    Update contact_serializers.py

--- accounts/serializers/contact_serializers.py
+++ accounts/serializers/contact_serializers.py
@@ -4,13 +4,5 @@ from rest_framework import serializers
 class ContactSupportSerializer(serializers.Serializer):
     full_name = serializers.CharField(max_length=100)
     contact_email = serializers.EmailField()
-    concern_type = serializers.ChoiceField(
-        choices=[
-            "Account Access Issue",
-            "Password Reset Problem",
-            "Two-Factor Authentication",
-            "Account Locked",
-            "Other",
-        ]
-    )
-    message = serializers.CharField(max_length=300)
+    concern_type = serializers.CharField(max_length=100)
+    message = serializers.CharField(max_length=2000)
